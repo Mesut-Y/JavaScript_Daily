38513bc536ee178f14f477381e8af0efdef1210b
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-03-06 22:43:23 +0300

    arrays and basic foreach example

---
+++ 9_Arrays.js
@@ -0,0 +1,15 @@
+let array1=[0,1,2,3,4,5,6,7,8,9];
+
+let array2=[0,"Mesut",null,3.14,'?',undefined,"Y",true];
+
+console.log(array2);
+console.log(array2[7]);
+console.log(array2[array2.length-1]);
+
+let array3 = new Array("Elif", "Su","Kemal","Namık", "Ateş");
+array3.forEach(function(item){
+    console.log(item);
+});
+
+//***************** Functions Of Arrays ******************//
+

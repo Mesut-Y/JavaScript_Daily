24de3eb7d81e0846c55819797a258d5888472ae4
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-03-07 14:54:58 +0300

    array filtreleme project başlandı.

---
+++ 9_Arrays2.js
@@ -0,0 +1,30 @@
+let watch1 = {
+    brand : "Fossil",
+    model : "FFS6042",
+    price : 9009.00
+}
+let watch2 = {
+    brand : "Emporio Armani",
+    model : "AR11360",
+    price : 21195.00
+}
+let watch3 = {
+    brand : "Cerruti 1881",
+    model : "CIWGR0012301",
+    price : 17068.00
+}
+let watch4 = {
+    brand : "Tommy Hilfiger",
+    model : "TH1710541",
+    price : 8799.00
+}
+let watch5 = {
+    brand : "Fossil",
+    model : "FFS5251SET",
+    price : 13041.00
+}
+let watch6 = {
+    brand : "Emporio Armani",
+    model : "AR11215",
+    price : 22167.00
+}

85371457f1e9996bfc19eff26b0628247f131b45
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-03-10 23:58:46 +0300

    selector second way
    querySelector
    querySelectorAll

--- 2_DomSelectors.js
+++ 2_DomSelectors.js
@@ -30,7 +30,32 @@ getElemByTagName.forEach(function(item){
 //console.log(getElemById);
 //console.log(getElemByClassName);
 
+// ? Second Way: querySelector  ,   querySelectorAll
 
+
+const clearButton = document.querySelector("#todoClearButton");  //use # for id like css
+console.log(clearButton);
+//console.log(document.getElementById("todoClearButton"));
+
+
+const itemList = document.querySelector(".list-group-item"); //use . for id like css
+console.log(itemList); //! It is used querySelectorAll for all elements in class
+
+const itemListTrue = document.querySelectorAll(".list-group-item");
+console.log(itemListTrue);
+console.log("gruptaki 3. eleman: ",document.querySelectorAll(".list-group-item")[2]);
+console.log("gruptaki son eleman: ",document.querySelectorAll(".list-group-item")
+                [document.querySelectorAll(".list-group-item").length-1]);
+
+let childItem = document.querySelectorAll("li:nth-child(3)"); //get 3rd child
+console.log(childItem);
+
+childItem = Array.from(document.querySelectorAll("li:nth-child(even)")); //çift sayılar.  odd-tek sayılar
+console.log(childItem);
+
+childItem.forEach(function (item){
+    item.style.backgroundColor="lightgrey";
+})
 
 
 

e519bad80a2717750ae5e8b5c5d8d12a7e270fdf
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-03-11 23:31:52 +0300

    style özelliği uygulandı.

--- 1_dom1.js
+++ 1_dom1.js
@@ -5,4 +5,6 @@ value = document.links.item(4).classList[0];
 value = document.forms.item(1).id;
 
 
+
+
 console.log(value);
---
+++ 3_Style.js
@@ -0,0 +1,16 @@
+const todo1 = document.querySelector(".list-group-item");
+const button1 = document.querySelector("#todoClearButton");
+
+todo1.style.backgroundColor = "lightgrey";
+todo1.style.padding = "30px";
+todo1.style.paddingLeft = "50px";
+todo1.style.fontWeight = "900";
+
+
+
+button1.style.backgroundColor = "purple";
+button1.style.padding = "30px";
+button1.style.borderRadius = "50px";
+
+console.log(todo1);
+console.log(button1);

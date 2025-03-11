4b3e9cb2a6f38dc62e3ee81e836835d13c4cab9f
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-03-11 23:55:09 +0300

    seçilen elemente style uygulandı.

--- 4_ElementHeredity.js
+++ 4_ElementHeredity.js
@@ -10,10 +10,13 @@ todoTitle = todoTitle.children[3].children[0];
 todoTitle.style.fontWeight = "900";
 todoTitle.style.fontSize = "40px";
 
-todo3 = todo3.children[0].nextElementSibling.nextElementSibling; //previousElementSibling -> önceki
-
 container = container[0].parentElement.parentElement;
 
+todo3 = todo3.children[0].nextElementSibling.nextElementSibling; //previousElementSibling -> önceki
+todo3.innerHTML = "<b> Todo değişti.</b>"
+todo3.style.color = "red";
+
+
 console.log(todoTitle);
-console.log(todo3);
 console.log(container);
+console.log(todo3);

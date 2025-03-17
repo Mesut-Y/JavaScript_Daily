3ac7efe429d573ff1abe37d95beedc39155b45f8
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-03-17 23:19:43 +0300

    Todo List project elementler seçildi.

---
+++ app.js
@@ -0,0 +1,8 @@
+const todoBody1 = document.querySelectorAll(".card-body")[0];
+const todoInput = document.querySelector("#todoName");
+const todoAddButton = document.querySelector("#todoAddButton");
+const todoListGroup = document.querySelector(".list-group");
+const todoClearButton = document.querySelector("#todoClearButton");
+
+
+console.log(todoAddButton);

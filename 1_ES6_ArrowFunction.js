83166ac6619ad205d49292a36d8ee231605e432c
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-03-21 21:56:53 +0300

    ES6 Arrow Function uygulandı.

---
+++ 1_ES6_ArrowFunction.js
@@ -0,0 +1,28 @@
+function write(){
+    console.log("Merhaba");
+}
+
+write();
+
+function writeWithParam(firstName){
+    console.log("Merhaba",firstName);
+}
+
+writeWithParam("Mesut");
+
+const basicArrowFunction = firstName => console.log("Merhaba",firstName);
+
+basicArrowFunction("Mesut");
+
+const calculate = (num1, num2) =>{
+    console.log("Toplam",num1+num2);
+    console.log("Fark",num1-num2);
+    console.log("Çarpım",num1*num2);
+    console.log("Bölüm",num1/num2);
+}
+
+calculate(10,6);
+
+let x=4;
+const calculateCube = x => x*x*x; //tek parametrede () ve tek satırda {return } kullanılmayabilir.
+console.log(x,"sayısının küp işlemi: ", calculateCube(x));

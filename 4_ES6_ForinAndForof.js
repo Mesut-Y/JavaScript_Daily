9d842a6d5d22cbbe8c3c912590a68a377a2754f8
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-03-22 23:46:52 +0300

    ES6 for of uygulandı.

--- 4_ES6_ForinAndForof.js
+++ 4_ES6_ForinAndForof.js
@@ -8,6 +8,12 @@ names.forEach((item)=>{
 
 
 console.log("-------------For in --------------------");
-for (let i in names){ //in kodu ile dizi indexi döner
+for (let i in names){ //in kodu ile dizi indexi döner.
     console.log("Dizinin ", i, ".elemanı: ", names[i]);
+}
+
+
+console.log("-------------for of --------------------");
+for (let j of names){ //of kodu ile dizi itemleri döner.
+    console.log("Dizinin ", names.indexOf(j), ".elemanı: ", j);
 }

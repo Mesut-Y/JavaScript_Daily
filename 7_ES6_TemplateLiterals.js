7ed6b65eb8b160014d6c2a5be0b6b9bd258385cf
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-03-23 17:49:01 +0300

    ES6 literal template uygulandı.

---
+++ 7_ES6_TemplateLiterals.js
@@ -0,0 +1,17 @@
+const func1 = (firstName, secondName) =>{
+    console.log("Eski Yöntem => İsim: " + firstName + " Soyisim: " + secondName);
+    console.log(`Template Literal => İsim: ${firstName} Soyisim: ${secondName}`);
+    console.log(`Template Literal2 => 
+        İsim: ${firstName} 
+        Soyisim: ${secondName}`);
+}
+
+console.log(func1("Tuna", "Tan"));
+
+
+function getUserById(userId){
+    //fetch(`http://localhost:8080/users/${userId}`)
+    console.log(`http://localhost:8080/users/${userId}`);
+}
+
+getUserById(42);

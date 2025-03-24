bade00a0247bf9dcb97bcbf36c713752825f6d13
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-03-24 23:06:16 +0300

    ES6 OOP Inheritance 2 uygulandı.
    super()
    super
    this

--- 9_ES6_Inheritance.js
+++ 9_ES6_Inheritance.js
@@ -20,11 +20,28 @@ class Car extends Vehicle{
     }
 }
 
+class Motorcycle extends Vehicle{
+    
+    constructor(brand,model,color){
+        super(brand, model);
+        this.color = color;
+    }
+    getColor(){
+        return this.color;
+    }
+}
+
 let car1 = new Car("BMW", "X2", 2020);
+let motor1 = new Motorcycle("Honda", "CL250", "blue");
 
-
+console.log("----------- vehicle sınıfından kalıtım almış Car sınıfı nesnesi ------------");
 console.log(car1);
 car1.movement(250);
 console.log(`${car1.brand} aracınızın trafiğe çıkış yılı: ${car1.getYear()}`);
+
+console.log("----------- vehicle sınıfından kalıtım almış Motorscyle sınıfı nesnesi ------------");
+console.log(motor1);
+motor1.movement(200);
+console.log(`${motor1.brand} aracınızın trafiğe çıkış yılı: ${motor1.getColor()}`);
 
 

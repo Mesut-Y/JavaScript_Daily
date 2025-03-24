58440413d9ff48e82414b28360fed430a7c1b3de
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-03-24 20:23:41 +0300

    ES6 OOP Static uygulandı.

--- 8_ES6_OOP_Nesne.js
+++ 8_ES6_OOP_Nesne.js
@@ -1,3 +1,4 @@
+let carCounter = 0;
 class Car{ //OOP yapısı C#, java kadar değil.
 
     /*
@@ -5,12 +6,17 @@ class Car{ //OOP yapısı C#, java kadar değil.
     2-Constructor
     3-Functions
     */
+    
+    static countCar(){ //static fonksiyon ve attribute lere sadece class ulaşabilir.
+        return carCounter;
+    }
 
     constructor(brand, model, year, color){ //A class may only have one constructor
         this.brand = brand;
         this.model = model;
         this.year = year;
         this.color = color;
+        carCounter++;
     }
     writeAllInfo(){
         console.log(this.brand, this.model, this.year, this.color)
@@ -20,11 +26,14 @@ class Car{ //OOP yapısı C#, java kadar değil.
     }
 }
 
-let car1 = new Car("Toyota", "Corolla", 2014, "Red");
+let car1 = new Car("Toyota", "Corolla", 2014, "Velvet Red");
 let car2 = new Car("Fiat", "Egea Cross", 2018, "Parlament Blue");
+let car3 = new Car("Kia", "Sportage", 2019, "Petrol Green");
 
 car1.writeAllInfo();
 console.log(car2.getBrand()," ",car2.model);
 console.log(`İkinci araba bilgileri:
         Markası: ${car2.getBrand()} 
         Modeli ${car2.model}`);
+
+console.log(Car.countCar());

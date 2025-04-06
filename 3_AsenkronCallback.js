1f937fca2b2d00a2ec47bb0773869d406d5d3e40
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-04-06 23:23:11 +0300

    JavaScript Asenkron_Callback çoklu parametre uygulandı.

--- 3_AsenkronCallback.js
+++ 3_AsenkronCallback.js
@@ -4,19 +4,21 @@
 function getName(callback){
     setTimeout(() => {
         let name = "Mesut" // web servisten gelmiş örneği
-        callback(name);
+        callback(name, "Sayın", 56);
     }, 1000);
 
 }
 
-function getSurname(name){
+function getSurname(name, hitap, counter){
     setTimeout(() => {
         let surname = "Y"; //web servisten gelmiş örneği
-        console.log(`Hoşgeldiniz ${name} ${surname}`);
+        console.log(
+`Hoşgeldiniz ${hitap} ${name} ${surname},
+Günlük giriş yapan sayısı: ${counter} `);
     }, 500);
 }
 
 // getName(); //1000ms
 // getSurname(); //500ms
 
-getName(getSurname);
+getName(getSurname); //()kullanılmıyor.

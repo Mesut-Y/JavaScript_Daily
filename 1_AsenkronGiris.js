197cbcf95de6eebb925d511ac490d0d10c490f9f
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-04-06 21:19:19 +0300

    JavaScript Asenkron çalışmaslar_1 uygulandı.

---
+++ 1_AsenkronGiris.js
@@ -0,0 +1,21 @@
+/*
+Asenkron Örnekleri:
+1-Timing
+2-Events
+3-HTTP İstekleri
+
+----- Asenkron Kontrolü için => CALLBACK - PROMISE - ASYNC & AWAIT 
+*/
+
+setTimeout(()=>{    //asenkron komutlar kendi zamanlamasına(aniden veya süreli) göre çalışır.
+    console.log("1000ms çalışır.")
+},1000);
+
+setTimeout(()=>{
+    console.log("500ms çalışır.")
+},500);
+
+
+console.log("Senkron komut 1") //senkron çalışanlar normal sırada çalışır.
+
+console.log("Senkron komut 2")

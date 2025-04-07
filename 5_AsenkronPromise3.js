a3fdd48dfa521e2f356f8d9df9c6e31a32549f63
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-04-07 14:05:58 +0300

    JavaScript Asenkron_Promise-XMLHttpRequest(jsonplaceholder) uygulandı.

---
+++ 5_AsenkronPromise3.js
@@ -0,0 +1,23 @@
+// Promise  XMLHttpRequest(https://jsonplaceholder.typicode.com/users)
+
+function getUsers(url){
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange",() => {
+            try {
+                if (xhr.readyState === 4 && xhr.status === 200){
+                    resolve(JSON.parse(xhr.responseText));
+                } 
+            } catch (error) {
+                reject(error);
+            }         
+        })
+        xhr.open("GET",url);
+        xhr.send();
+    })
+}
+
+getUsers("https://jsonplaceholder.typicode.com/users")
+.then(data => { console.log(data)})
+.catch(err => {console.log(err)})
+.finally(() => { console.log("finally");})

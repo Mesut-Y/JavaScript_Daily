dc81befcdc9d7c8076abf866364f79e7b2ec1871
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-04-07 16:35:51 +0300

    JavaScript Asenkron_Promise-XMLHttpRequest(jsonplaceholder)-3 uygulandı
    user id bilgisi kullanılarak comments getirildi.

--- 5_AsenkronPromise4.js
+++ 5_AsenkronPromise4.js
@@ -1,5 +1,56 @@
-// Promise  XMLHttpRequest(https://jsonplaceholder.typicode.com/users/id)
+// Promise  XMLHttpRequest(https://jsonplaceholder.typicode.com/users/id), (https://jsonplaceholder.typicode.com/comments)
 
-function getUser(){
-    
+function createUrl(url, id) {
+    if (id != null)
+        return `${url}/${id}`;
+    else
+        return url;
 }
+
+function getUser(url, id) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange", () => {
+            try {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                }
+            } catch (error) {
+                reject(error);
+            }
+        });
+        let newUrl = createUrl(url, id);
+        xhr.open("GET", newUrl);
+        xhr.send();
+
+    });
+}
+
+function getCommentsById(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange", () => {
+            try {
+                if (xhr.status === 200 && xhr.readyState === 4) {
+                    resolve(JSON.parse(xhr.responseText));
+                }
+            } catch (error) {
+                reject(error);
+            }
+        })
+        xhr.open("GET", url);
+        xhr.send();
+    })
+}
+
+getUser("https://jsonplaceholder.typicode.com/users", 2)
+    .catch(err => { console.log(err); })
+    .then(data => {
+        console.log(data);
+        // getCommentsById(`https://jsonplaceholder.typicode.com/comments/${data.id}`)
+        // .then(data => console.log(data));
+        return getCommentsById(`https://jsonplaceholder.typicode.com/comments/${data.id}`)
+
+    })
+    .then(data => console.log(data))
+    .finally(() => console.log("finally"));

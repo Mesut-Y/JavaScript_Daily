2a03830b60e7e915abfcb7cc865c7847e721b60a
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-04-07 12:43:33 +0300

    JavaScript Asenkron_Promise2 uygulandı

--- 5_AsenkronPromise.js
+++ 5_AsenkronPromise.js
@@ -1,12 +1,26 @@
-// PROMISE pending, resolve(fullfilled) .then, reject .catch
+// PROMISE pending, resolve(fulfilled) .then, reject .catch
 
 let check = true;
 
 const promise1 = new Promise((resolve, reject) => {
-    if(check)
-        resolve("Promise başarılı");
+    if (check)
+        resolve("Promise1 başarılı");
     else
-        reject("Promise başarısız");
+        reject("Promise1 başarısız");
 })
 
 console.log(promise1);
+
+function createPromise() {
+    return new Promise((resolve, reject) => {
+        if (check)
+            resolve("createPromise ile promise2 başarılı.");
+        else
+            reject("createPromise ile promise2 başarısız");
+    })
+}
+
+createPromise()
+    .then((response) => { console.log(response); })
+    .catch((error) => { console.log(error); })
+    .finally(() => { console.log("her zaman çalışır.") });

4172974a8562ea59639b8755c4711dc8f5625227
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-04-07 23:27:36 +0300

    javaScript Asenkron_Promise FetchApi-2 uygulandı

--- 6_AsenkronFetchAPI.js
+++ 6_AsenkronFetchAPI.js
@@ -1,4 +1,4 @@
-//  ---FETCH API---
+//  ---FETCH API---  (local) (https://jsonplaceholder.typicode.com/posts)
 
 // callback -> promise
 // Ajax(xmlhttprequest) -> fetch api
@@ -11,3 +11,29 @@ function getStudents(url) {
 }
 
 getStudents("students.json");
+
+function getData(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            for(let content of data){
+                if(content.userId === 9){
+                    console.log(content.id);
+                    console.log(content.title)
+                }
+            }
+        })
+        .catch(err => console.log("1.yöntem hatası"));
+}
+
+getData("https://jsonplaceholder.typicode.com/posts");
+
+function getData2(url){
+    return fetch(url);
+}
+
+getData2("https://jsonplaceholder.typicode.com/posts")
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(err => console.log("2.yöntem hatası"));

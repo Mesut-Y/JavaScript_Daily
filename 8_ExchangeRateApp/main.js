4a11fa0e0da523e8c4ac4f1076d6ae77599cc48f
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-04-09 20:06:25 +0300

    dom ile nesnelere erişim sağlandı.

---
+++ 8_ExchangeRateApp/main.js
@@ -0,0 +1,18 @@
+const inputMoney = document.querySelector("#amountOfMoney");
+const firstOption = document.querySelector("#firstCurrencyOptions");
+const secondOption = document.querySelector("#secondCurrencyOptions");
+const resultMoney = document.querySelector("#result");
+
+runEventListeners();
+
+function runEventListeners(){
+    inputMoney.addEventListener("input",exchange);
+
+}
+
+function exchange(){
+    const amount = Number(inputMoney.value.trim());
+    const firstOptionValue = firstOption.options[firstOption.selectedIndex].textContent;
+    const secondOptionValue = secondOption.options[secondOption.selectedIndex].textContent;
+    console.log(amount, firstOptionValue, secondOptionValue);
+}
